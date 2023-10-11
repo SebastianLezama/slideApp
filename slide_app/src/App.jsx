22a14f1357ec49bc8c29c96ab57d4936c32c23c3
SebastianLezama <sebastian.lezama@gmail.com>
2023-10-10 23:42:32 -0300

    gapi client

--- slide_app/src/App.jsx
+++ slide_app/src/App.jsx
@@ -10,6 +10,9 @@ import  wp7  from './assets/wp7.jpg'
 import  wp8  from './assets/wp8.jpg'
 import  wp9  from './assets/wp9.jpg'
 
+// import gapi from 'gapi-client';
+import { gapi } from 'gapi-script';
+
 function App() {
   // TODO make randomizer to img obj
   // TODO fetch from drive folder
@@ -43,6 +46,67 @@ function App() {
     }, 9000);
     return () => clearInterval(slider);
   }, [index]);
+
+  const API_KEY = import.meta.env.VITE_G_API_KEY2
+  // const DISCOVERY_DOC = import.meta.env.DRIVE_ID
+  const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+  const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
+  const GOOGLE_ID = import.meta.env.GOOGLE_ID
+  const SCOPES = 'https://www.googleapis.com/auth/drive.readonly';
+
+  const getFotosFromDrive = async ( ) => {
+    async function initializeGapiClient() {
+      await gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        scope: SCOPES,
+      }).then(async function() {
+        return await gapi.client.request({
+          path: SCOPES,
+          discoveryDocs: DISCOVERY_DOC,
+          
+        });
+    })
+    .then(
+      function () {
+        tokenClient = google.accounts.oauth2.initTokenClient({
+          client_id: CLIENT_ID,
+          scope: SCOPES,
+          // callback: '', // defined later
+        });}
+      // function (error) {}
+    );
+    }
+    
+    await gapi.load('client', initializeGapiClient);
+  };
+
+  useEffect(() => {
+
+    getFotosFromDrive()
+  
+      
+
+    
+  }, [])
+
+  // useEffect(() => {
+  //   gapi.load('client:auth2', initClient);
+
+  //   function initClient() {
+  //     gapi.client.init({
+  //       discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+  //       clientId: GOOGLE_ID,
+  //       scope: 'https://www.googleapis.com/auth/drive.metadata.readonly'
+  //     }).then(function () {
+  //       // do stuff with loaded APIs
+  //       console.log('it worked');
+  //     });
+  //   }
+  
+    
+  // }, [])
+  
 
   return (
     <section className="section">
